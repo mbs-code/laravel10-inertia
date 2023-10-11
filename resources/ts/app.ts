3b2cc453bce83ba20fc0ba9871077f080de37511
mbs-code <mbscodec@gmail.com>
2023-10-12 00:15:53 +0900

    add: pinia

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -6,6 +6,8 @@ import { createApp, h, DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import PrimeVue from 'primevue/config'
 import Tailwind from 'primevue/passthrough/tailwind'
 import LocaleJa from './ja.json'
@@ -16,10 +18,14 @@ createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
   setup ({ el, App, props, plugin }) {
+    const pinia = createPinia()
+    pinia.use(piniaPluginPersistedstate)
+
     createApp({ render: () => h(App, props) })
       .use(plugin)
       // eslint-disable-next-line no-undef
       .use(ZiggyVue, Ziggy)
+      .use(pinia)
       .use(PrimeVue, {
         locale: LocaleJa,
         unstyled: true,
